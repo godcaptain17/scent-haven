58935f8da9622e06651fec323aa9784271066095
godcaptain17 <danieliseolua@gmail.com>
2026-05-24 11:26:34 +0100

    Update ui.js

--- ui.js
+++ ui.js
@@ -40,7 +40,7 @@ async function renderNavbar(activePage = '') {
   
   nav.innerHTML = `
     <a href="/index.html" class="nav-logo">
-      <span class="nav-logo-text"><img src="https://vudxovvqpjdhlemugtqh.supabase.co/storage/v1/object/public/assets/logo.png" alt="Scent Haven" style="height:65px;width:auto;vertical-align:middle;margin-right:8px;"> <span style="display:none">Scent Haven</span></span>
+      <span class="nav-logo-text"><img src="https://vudxovvqpjdhlemugtqh.supabase.co/storage/v1/object/public/assets/logo.png" alt="Scent Haven" style="height:44px;width:auto;vertical-align:middle;"> <span style="display:none">Scent Haven</span></span>
     </a>
     <ul class="nav-links">
       <li><a href="/index.html" class="nav-link ${activePage === 'home' ? 'active' : ''}">Home</a></li>
